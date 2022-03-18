bd5a12a26618dfcc0f004c74559ddfc493b2cdfe
Sevki <sevki.oksuz@gmail.com>
2022-03-18 09:52:24 +0100

    Patch request done

--- src/test/java/Day8/PUTRequestDemo.java
+++ src/test/java/Day8/PUTRequestDemo.java
@@ -15,4 +15,29 @@ public class PUTRequestDemo {
     public void beforeclass() {
         baseURI = ConfigurationReader.get("spartan_api_url");
     }
+
+    @Test
+    public void test1(){
+        //Create one map for the put request json body
+        Map<String,Object> putRequestMap = new HashMap<>();
+        putRequestMap.put("name","PutName");
+        putRequestMap.put("gender","Male");
+        putRequestMap.put("phone",1231312321l);
+
+        given().log().all()
+                .and()
+                .contentType(ContentType.JSON)
+                .and()
+                .pathParam("id",90)
+                .and()
+                .body(putRequestMap).
+                when()
+                .put("/api/spartans/{id}")
+                .then().log().all()
+                .assertThat().statusCode(204);
+
+
+        //send get request to verify body
+
+    }
 }
